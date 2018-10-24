44247b6706f39883820c6c30160e6400a8837d01
Noam Ross <noam.ross@gmail.com>
2018-10-24 18:50:45 -0400

    cut to a single line of code

--- redirectr.js
+++ redirectr.js
@@ -1,6 +1,2 @@
-function getPostURL() {
-  return document.evaluate("string(//div[contains(., 'This article was first published on')]/strong/a/@href)", document, null, XPathResult.stringValue, null).stringValue;
-}
-
-window.location = getPostURL()
+window.location = document.evaluate("string(//div[contains(., 'This article was first published on')]/strong/a/@href)", document, null, XPathResult.stringValue, null).stringValue;
 
