5571a1b26aec1018315e5d4c078a159768689e85
Noam Ross <noam.ross@gmail.com>
2018-11-10 17:33:00 -0500

    v2 for faster redirection

--- redirectr.js
+++ redirectr.js
@@ -1,2 +1,14 @@
-window.location = document.evaluate("string(//div[contains(., 'This article was first published on')]/strong/a/@href)", document, null, XPathResult.stringValue, null).stringValue;
+function waitForElementToDisplay(selector, time) {
+        if(document.querySelector(selector)!=null) {
+          window.location = document.evaluate("string(//div[contains(., 'This article was first published on')]/strong/a/@href)", document, null, XPathResult.stringValue, null).stringValue;
+        }
+        else {
+            setTimeout(function() {
+                waitForElementToDisplay(selector, time);
+            }, time);
+        }
+    }
+
+waitForElementToDisplay("div.entry > div:nth-child(3)", 10)
+
 
